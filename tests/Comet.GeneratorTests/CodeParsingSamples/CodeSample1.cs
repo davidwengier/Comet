196c3ba3f8fa7567321565853d310928a49fa3a0
David Wengier <git@wengier.com>
2020-05-29 11:57:48 +1000

    Make code sample 1 actually different from code sample 2 :P

--- tests/Comet.GeneratorTests/CodeParsingSamples/CodeSample1.cs
+++ tests/Comet.GeneratorTests/CodeParsingSamples/CodeSample1.cs
@@ -1,37 +1,15 @@
 ﻿using Comet;
 
 public class MyView : View
-{ 
-	State<int> count = 0; 
-	State<bool> shouldCelebrate = false; 
-
-	[Environment]
-	public readonly string Foo;
-
-	//[Body]
-	//View body() => new Text(()=>$"{count} times");
-
-	//[Body]
-	//View body() => new VStack
-	//{
-	//	//(shouldCelebrate ?(View)new Text("Celebrate!!!!") : new Spacer()),
-	//	new Text(()=>$"{count} times"),
-	//	new Button("Click Me!", ()=> count.Value++),
-	//};
+{
+	State<int> count = 0;
+	State<bool> shouldCelebrate = false;
 
 	[Body]
-	View body()
+	View body() => new VStack
 	{
-		return new VStack
-		{
-			//(shouldCelebrate ?(View)new Text("Celebrate!!!!") : new Spacer()),
-			new Text(()=>$"{count} times"),
-			new Button("Click Me!", ()=> count.Value++),
-		};
-	}
-
-	public MyView(object MyModel)
-	{
-		Body = () => new Text(MyModel.ToString());
-	}
+		(shouldCelebrate ?(View)new Text($"{count.Value}") : new Spacer()),
+		new Text(()=>$"{count} times"),
+		new Button("Click Me!", ()=> count.Value++),
+	};
 }
